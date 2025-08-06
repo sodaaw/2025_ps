a5a34cf4186ad2850a8a8c69c96fe78200472cdb
sodaaw <ghk132435@naver.com>
2025-08-06 23:13:30 +0900

    25/08/06

---
+++ aug_week2/11006.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        int n, m;
+        cin >> n >> m;
+
+        cout << 2*m - n << " " << n - m << endl;
+    }
+
+    return 0;
+}
