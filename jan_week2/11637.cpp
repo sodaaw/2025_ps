1a5aa1cf43e396e88026501be1fbbbf41caeb7ce
sodaaw <ghk132435@naver.com>
2026-02-16 15:18:12 +0900

    26/02/16

--- jan_week2/11637.cpp
+++ jan_week2/11637.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     int t;
     cin >> t;
 
-    for (int i = 0; i < t; i++) {
+    while (t--) {
         int n;
         cin >> n;
 
-        int v[n];
+        vector<int> v(n);
+        int sum = 0;
+
         for (int j = 0; j < n; j++) {
             cin >> v[j];
+            sum += v[j];
         }
 
         int maxV = v[0];
+        int winner = 1;
+
         for (int j = 1; j < n; j++) {
-            if (v[j] > v[j - 1]) maxV = v[j];
+            if (v[j] > maxV) {
+                maxV = v[j];
+                winner = j + 1;
+            }
         }
 
-        if (maxV )
+        int countMax = 0;
+        for (int j = 0; j < n; j++) {
+            if (v[j] == maxV) countMax++;
+        }
+
+        if (countMax > 1) cout << "no winner\n";
+        else {
+            if (maxV > sum / 2) cout << "majority winner " << winner << endl;
+            else cout << "minority winner " << winner << endl;
+        }
     }
 
     return 0;
